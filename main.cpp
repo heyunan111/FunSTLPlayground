eaff0c00f298edae985c0a49056449d7a1b32973
hyn <he_yu_nan@qq.com>
2023-07-15 21:54:28 +0800

    any

--- main.cpp
+++ main.cpp
@@ -1,20 +1,7 @@
-#include <iostream>
-#include <type_traits>
-
+#include "src/any.hpp"
 int main() {
-  using Type1 = std::decay_t<int>;         // int
-  using Type2 = std::decay_t<int &>;       // int
-  using Type3 = std::decay_t<int *>;       // int*
-  using Type4 = std::decay_t<const int *>; // const int*
-  using Type5 = std::decay_t<int[]>;       // int*
-  using Type6 = std::decay_t<int(int)>;    // int(*)(int)
 
-  std::cout << std::is_same_v<int, Type1> << std::endl;          // 输出: 1
-  std::cout << std::is_same_v<int, Type2> << std::endl;          // 输出: 1
-  std::cout << std::is_same_v<int *, Type3> << std::endl;        // 输出: 1
-  std::cout << std::is_same_v<const int *, Type4> << std::endl;  // 输出: 1
-  std::cout << std::is_same_v<int *, Type5> << std::endl;        // 输出: 1
-  std::cout << std::is_same_v<int (*)(int), Type6> << std::endl; // 输出: 1
+  any_test();
 
   return 0;
 }
--- src/any.hpp
+++ src/any.hpp
@@ -1,53 +1,70 @@
 #pragma once
 
+#include <cstddef>
 #include <iostream>
 #include <memory>
 #include <typeinfo>
 
 namespace hyn {
 
-// 使用any_cast时发生异常
 class bad_any_cast : public std::bad_cast {
 public:
-  const char *what() const noexcept override {
-    return "bad any cast: failed conversion using hyn::any_cast";
-  }
+  virtual const char *what() const noexcept { return "类型不匹配"; }
 };
 
 class any {
-public:
-  constexpr any() = default;
-  ~any() = default;
-
-  any(const any &) = delete;
-  any &operator=(const any &) = delete;
-
-  any(any &&) = default;
-  any &operator=(any &&) = default;
-
-  template <typename T> any(T data) : ptr_(std::make_unique<derive<T>>(data)) {}
-
-  void reset() noexcept { ptr_.reset(); }
-
-  void swap(any &rhs) noexcept { ptr_.swap(rhs.ptr_); }
-
-private:
-  class base {
+  class base_ {
   public:
-    virtual ~base() = default;
+    virtual ~base_() = default;
   };
 
-  template <typename T> class derive {
+  template <typename T> class derive_ : public base_ {
   public:
-    derive(T data) : data_(data) {}
-
+    derive_(T data) : data_(data) {}
     T data_;
   };
 
+public:
+  any() = default;
+  ~any() = default;
+
+  any(const any &) = delete;
+
+  any &operator=(const any &) = delete;
+
+  template <typename T>
+  any(T data) : ptr_(std::make_unique<derive_<T>>(data)) {}
+
+  any(any &&) = default;
+
+  any &operator=(any &&) = default;
+
+  template <typename T> T _Cast() {
+    auto *p = dynamic_cast<derive_<T> *>(ptr_.get());
+    if (p == nullptr)
+      throw bad_any_cast{};
+    return p->data_;
+  }
+
 private:
-  std::unique_ptr<base> ptr_;
+  std::unique_ptr<base_> ptr_;
 };
 
-void swap(any &lhs, any &rhs) { lhs.swap(rhs); }
+template <typename T> T any_cast(any &rhs) { return rhs._Cast<T>(); }
 
 } // namespace hyn
+
+struct A {
+  A(int a) : s(a) {}
+  int s;
+};
+
+void any_test() {
+  hyn::any a(12);
+  std::cout << hyn::any_cast<int>(a) << std::endl;
+  hyn::any b(3.14);
+  std::cout << hyn::any_cast<double>(b) << std::endl;
+  A aa(2);
+  hyn::any c(aa);
+  std::cout << hyn::any_cast<A>(c).s << std::endl;
+}
